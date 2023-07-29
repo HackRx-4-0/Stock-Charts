c92338eacfcdb306dec11b65efb00729c1fe1dca
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2023-07-30 00:51:04 +0530

    frontend changes

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -4,19 +4,15 @@ const MyComponent = () => {
   return (
     <nav className="bg-[#050713] p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">Bajaj Finserv</div>
+        <div className="text-white font-bold text-xl">
+          <a href="/" className="text-white px-3 py-2 rounded">
+            Stock Charts
+          </a>
+        </div>
         <ul className="flex space-x-4">
           <li>
             <a
-              href="./"
-              className="text-white hover:bg-gray-700 px-3 py-2 rounded"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="./"
+              href="/"
               className="text-white hover:bg-gray-700 px-3 py-2 rounded"
             >
               About
@@ -24,7 +20,7 @@ const MyComponent = () => {
           </li>
           <li>
             <a
-              href="./"
+              href="/"
               className="text-white hover:bg-gray-700 px-3 py-2 rounded"
             >
               Services
@@ -32,7 +28,7 @@ const MyComponent = () => {
           </li>
           <li>
             <a
-              href="./"
+              href="/"
               className="text-white hover:bg-gray-700 px-3 py-2 rounded"
             >
               Contact
